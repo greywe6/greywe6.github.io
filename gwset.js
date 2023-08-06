abde707ccbb927fa7080bd1e3767a0c9ac3d662e
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-06 23:16:20 +0300

    Update gwset.js

--- gwset.js
+++ gwset.js
@@ -11,7 +11,6 @@
     Lampa.Storage.set('glass_opacity', 'blacked');
     Lampa.Storage.set('video_quality_default', '2160');
 	Lampa.Storage.set('player_launch_trailers', 'youtube');
-	Storage.get('player_size', 'default');
 	
     //Видимость постеров TMDB-Proxy
 	Lampa.Storage.set('tmdb_proxy_image', 'http://imagetmdb.com');
