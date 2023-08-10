c73d439bbd5a1be4cbfdb378a78a06a6b167c382
greywe6 <greyweb@mail.ru>
2023-08-11 00:37:12 +0300

    -

--- gwmd.js
+++ gwmd.js
@@ -181,7 +181,6 @@ Date.now||(Date.now=function(){return(new Date.getTime())}),function(){"use stri
 				if ($('body').find('.full-start__poster,.full-start-new__poster').length) {
 					$('.timeline, .card--last_view').remove();
 					$('body').find('.full-start__poster,.full-start-new__poster').append("<div class='card--last_view' style='top:0.6em;right: -.5em;position: absolute;background: #FF4242;color: #fff;padding: 0.3em 0.4em;font-size: 1.2em;-webkit-border-radius: 0.3em;-moz-border-radius: 0.3em;border-radius: 0.3em;'><div style='float:left;margin:-5px 0 -4px -4px' class='card__icon icon--history'></div>" + last_view +"</div>").parent().append('<div class="timeline" style="position:relative;"></div>');
-					$('body').find('.timeline').append(Lampa.Timeline.render(viewed.view));
 				}
 				if ($('body').find('.filter--sort').length) $('body').find('.files__left .time-line, .card--last_view').remove();
 			} else $('body').find('.timeline,.card--last_view').remove();
