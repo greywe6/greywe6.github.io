177eb5a5352f7eb7a37f1141c89d0d96c43b2c7a
greywe6 <greyweb@mail.ru>
2023-08-12 02:34:57 +0300

    -

--- gwset.js
+++ gwset.js
@@ -44,10 +44,10 @@
 	});
 
     //Убирает общую кнопку Онлайн и достаёт все кнопки из блока
-    Lampa.Listener.follow('full', function (e) {
+    /*Lampa.Listener.follow('full', function (e) {
     if (e.type == 'complite')
     $('.hide.buttons--container > div').prependTo('.full-start-new__buttons');
-    //$('.full-start__button.selector.button--play').remove();
-    });
+    $('.full-start__button.selector.button--play').remove();
+    });*/
 
 })();
