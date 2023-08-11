91abbbc52c93c6e3f1718fcfdb218ba19d57f2cc
greywe6 <greyweb@mail.ru>
2023-08-12 02:26:30 +0300

    -

--- gwset.js
+++ gwset.js
@@ -48,8 +48,6 @@
     if (e.type == 'complite')
     $('.hide.buttons--container > div').prependTo('.full-start-new__buttons');
     $('.full-start__button.selector.button--play').remove();
-    Navigator.focus(btn[0]);
-
     });
 
 })();
