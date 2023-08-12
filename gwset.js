449f28201d1fec8be1b14412effafb4b1ee36dd1
greywe6 <greyweb@mail.ru>
2023-08-12 13:47:30 +0300

    -

--- gwset.js
+++ gwset.js
@@ -47,7 +47,7 @@
     Lampa.Listener.follow('full', function (e) {
     if (e.type == 'complite') {
     $('.hide.buttons--container > div').prependTo('.full-start-new__buttons');
-    $('.full-start__button.selector.button--play').remove();
+    //$('.full-start__button.selector.button--play').remove();
     }
     });
 
