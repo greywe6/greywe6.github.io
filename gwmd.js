4e41610909cf444c26c3a5d0ad0fc1fe2e8dcd98
greywe6 <greyweb@mail.ru>
2023-08-13 02:18:15 +0300

    -

--- gwmd.js
+++ gwmd.js
@@ -2265,6 +2265,99 @@ Date.now||(Date.now=function(){return(new Date.getTime())}),function(){"use stri
 				}
 			});
 
+
+
+
+
+			Lampa.SettingsApi.addParam({
+				component: 'settings_modss',
+				param: {
+					name: 'modss_online_param',
+					type: 'static', //доступно select,input,trigger,title,static
+					default: true
+				},
+				field: {
+					name: '<div class="settings-folder" style="padding:0!important"><div style="width:1.8em;height:1.3em;padding-right:.5em"><svg viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32"><path d="m17 14.5 4.2-4.5L4.9 1.2c-.1-.1-.3-.1-.6-.2L17 14.5zM23 21l5.9-3.2c.7-.4 1.1-1 1.1-1.8s-.4-1.5-1.1-1.8L23 11l-4.7 5 4.7 5zM2.4 1.9c-.3.3-.4.7-.4 1.1v26c0 .4.1.8.4 1.2L15.6 16 2.4 1.9zM17 17.5 4.3 31c.2 0 .4-.1.6-.2L21.2 22 17 17.5z" fill="currentColor" fill="#ffffff" class="fill-000000"></path></svg></div><div style="font-size:1.3em">Online</div></div>'
+				},
+				onRender: function (item) {
+					if (Lampa.Storage.field('mods_onl')) {
+						item.show();
+					} else item.hide();
+					item.on('hover:enter', function () {
+						Lampa.Settings.create('modss_online_param');
+						Lampa.Controller.enabled().controller.back = function(){
+         		  Lampa.Settings.create('settings_modss');
+            }
+					});
+				}
+			});
+			Lampa.SettingsApi.addParam({
+				component: 'modss_online_param',
+				param: {
+					name: 'priority_balanser',
+					type: 'select', //доступно select,input,trigger,title,static
+					values: Modss.balansers(),
+					default: Modss.balansPrf
+				},
+				field: {
+					name: Lampa.Lang.translate('title_prioriry_balanser'),
+					description: Lampa.Lang.translate('title_prioriry_balanser_descr')
+				},
+				onRender: function (item) {
+					if (Lampa.Storage.field('mods_onl')) item.show(); 
+					else item.hide();
+				}, 
+				onChange: function (values) {
+				  var title = $('body').find('[data-name="priority_balanser"] .settings-param__value');
+			    title.text(title.text().split('<').shift());
+				}
+			});
+			Lampa.SettingsApi.addParam({
+				component: 'modss_online_param',
+				param: {
+					name: 'online_but_first',
+					type: 'trigger', //доступно select,input,trigger,title,static
+					default: true
+				},
+				field: {
+					name: Lampa.Lang.translate('title_online_first_but'),
+				},
+				onChange: function (item) {
+					Lampa.Storage.set('full_btn_priority', '');
+				},
+				onRender: function (item) {
+					if (Lampa.Storage.field('mods_onl')) item.show(); 
+					else item.hide();
+				}
+			});
+			Lampa.SettingsApi.addParam({
+				component: 'modss_online_param',
+				param: {
+					name: 'online_continued',
+					type: 'trigger', //доступно select,input,trigger,title,static
+					default: true
+				},
+				field: {
+					name: Lampa.Lang.translate('title_online_continued'),
+					description: Lampa.Lang.translate('title_online_descr')
+				},
+				onRender: function (item) {
+					if (Lampa.Storage.field('mods_onl')) item.show(); 
+					else item.hide();
+				}
+			});
+
+
+
+
+
+
+
+
+
+
+
+
 /* ТВИКИ */
 		Lampa.SettingsApi.addComponent({
 			component: 'settings_modss',
