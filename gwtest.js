cc4f25b14049a61d1540e9ff620866ff0276dfed
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-14 01:17:58 +0300

    Update gwtest.js

--- gwtest.js
+++ gwtest.js
@@ -5643,17 +5643,17 @@
     function kinopub(component, _object) {
       var network = new Lampa.Reguest();
       var extract = {};
+      var results = [];
+      var backend = 'http://back.freebie.tom.ru/lampa/kinopuburl?v=2010';
       var object = _object;
       var select_title = '';
-      Lampa.Storage.field('online_mod_prefer_http') === true;
-      var prox = component.proxy('kinopub');
-      var embed = prox + 'https://api.service-kp.com/v1/';
-      var token = Utils.decodeSecret([76, 91, 92, 0, 67, 85, 66, 68, 0, 95, 84, 92, 2, 11, 77, 64, 0, 3, 94, 91, 84, 68, 70, 83, 13, 92, 90, 79, 2, 78, 5, 5]);
+      var select_id = '';
+      var balanser_id = '';
       var filter_items = {};
       var choice = {
         season: 0,
         voice: 0,
-        voice_name: ''
+        quality: 0
       };
 
       function kinopub_api_search(api, callback, error) {
@@ -6305,6 +6305,60 @@
         balanser = last_bls[object.movie.id];
       }
 
+      this.changeBalanser = function (balanser_name) {
+        var last_select_balanser = Lampa.Storage.cache('online_mod_last_balanser', 3000, {});
+        last_select_balanser[object.movie.id] = balanser_name;
+        Lampa.Storage.set('online_mod_last_balanser', last_select_balanser);
+        var to  = this.getChoice(balanser_name);
+        var from = this.getChoice();
+        if(from.voice_name) to.voice_name = from.voice_name;
+        this.saveChoice(to, balanser_name);
+        Lampa.Activity.replace();
+      };
+    
+      this.createSource = function () {
+          var last_select_balanser = Lampa.Storage.cache('online_mod_last_balanser', 3000, {});
+  
+          if (last_select_balanser[object.movie.id]) {
+            balanser = last_select_balanser[object.movie.id];
+            Lampa.Storage.set('online_mod_last_balanser', last_select_balanser);
+          } else {
+            balanser = Lampa.Storage.get('online_mod_balanser', 'videocdn');
+          }
+  
+          if (!sources[balanser]) {
+            balanser = 'videocdn';
+          }
+  
+          return new sources[balanser](this, object);
+        };
+    
+      this.getChoice = function (for_balanser) {
+        var data = Lampa.Storage.cache('online_mod_choice_' + (for_balanser || balanser), 3000, {});
+        var save = data[selected_id || object.movie.id] || {};
+        Lampa.Arrays.extend(save, {
+          season: 0,
+          voice: 0,
+          voice_name: '',
+          voice_id: 0,
+          episodes_view: {},
+          movie_view: ''
+        });
+        return save;
+      };
+      this.extendChoice = function () {
+        extended = true;
+        source.extendChoice(this.getChoice());
+      };
+      this.saveChoice = function (choice, for_balanser) {
+        var data = Lampa.Storage.cache('online_mod_choice_' + (for_balanser || balanser), 3000, {});
+        data[selected_id || object.movie.id] = choice;
+        Lampa.Storage.set('online_mod_choice_' + (for_balanser || balanser), data);
+        var last_select_balanser = Lampa.Storage.cache('online_mod_last_balanser', 3000, {});
+        last_select_balanser[object.movie.id] = (for_balanser || balanser);
+        Lampa.Storage.set('online_mod_last_balanser', last_select_balanser);
+      };
+
       this.proxy = function (name) {
         var proxy1 = 'http://prox.lampa.stream/';
         var proxy2 = 'https://cors.nb557.workers.dev/';
@@ -6324,11 +6378,11 @@
 
       var sources = {
         videocdn: new videocdn(this, object),
-        rezka: new rezka(this, object),
-        rezka2: new rezka2(this, object),
+        cdnmovies: new cdnmovies(this, object),
         kinobase: new kinobase(this, object),
         collaps: new collaps(this, object),
-        cdnmovies: new cdnmovies(this, object),
+        rezka: new rezka(this, object),
+        rezka2: new rezka2(this, object),
         filmix: new filmix(this, object),
         anilibria: new anilibria(this, object),
         kodik: new kodik(this, object),
@@ -6344,7 +6398,7 @@
         voice: Lampa.Lang.translate('torrent_parser_voice'),
         source: Lampa.Lang.translate('settings_rest_source')
       };
-      var filter_sources = ['videocdn', 'rezka', 'rezka2', 'kinobase', 'collaps', 'cdnmovies', 'filmix', 'anilibria', 'kodik', 'kinopub', 'hdvb'];
+      var filter_sources = ['videocdn', 'cdnmovies', 'kinobase', 'collaps', 'rezka', 'rezka2', 'filmix', 'kinopub', 'hdvb'];
 
       if (Utils.isDebug()) {
         filter_sources.push('hdvb');
@@ -7011,6 +7065,7 @@
           reset: true
         });
         Lampa.Storage.set('online_mod_filter', choice);
+        add('source', 'Балансер');
         if (filter_items.voice && filter_items.voice.length) add('voice', Lampa.Lang.translate('torrent_parser_voice'));
         if (filter_items.season && filter_items.season.length) add('season', Lampa.Lang.translate('torrent_serial_season'));
         filter.set('filter', select);
@@ -7240,18 +7295,45 @@
 
 
       this.empty = function (msg) {
-        var empty = Lampa.Template.get('list_empty');
-        if (msg) empty.find('.empty__descr').text(msg);
-        scroll.append(empty);
+        var html = Lampa.Template.get('modss_does_not_answer', {});
+        html.find('.online-empty__buttons').remove();
+        html.find('.online-empty__title').text(Lampa.Lang.translate('empty_title_two'));
+        html.find('.online-empty__time').text(Lampa.Lang.translate('empty_text'));
+        scroll.append(html);
         this.loading(false);
       };
-      /**
-       * Показать пустой результат по ключевому слову
-       */
 
+      this.emptyForQuery = function () {
+        var _this6 = this;
 
-      this.emptyForQuery = function (query) {
-        this.empty(Lampa.Lang.translate('online_mod_query_start') + ' (' + query + ') ' + Lampa.Lang.translate('online_mod_query_end'));
+        this.reset();
+        var html = Lampa.Template.get('modss_does_not_answer', {
+          balanser: balanser
+        });
+        var tic = 10;
+        html.find('.cancel').on('hover:enter', function () {
+          clearInterval(balanser_timer);
+        });
+        html.find('.change').on('hover:enter', function () {
+          clearInterval(balanser_timer);
+          filter.render().find('.filter--sort').trigger('hover:enter');
+        });
+        scroll.append(html);
+        this.loading(false);
+        balanser_timer = setInterval(function () {
+          tic--;
+          html.find('.timeout').text(tic);
+
+          if (tic == 0) {
+            clearInterval(balanser_timer);
+            var keys = Lampa.Arrays.getKeys(sources);
+            var indx = keys.indexOf(balanser);
+            var next = keys[indx + 1];
+            if (!next) next = keys[0];
+            balanser = next;
+            if (Lampa.Activity.active().activity == _this6.activity) _this6.changeBalanser(balanser);
+          }
+        }, 1000);
       };
 
       this.getLastEpisode = function (items) {
@@ -7319,11 +7401,11 @@
         scroll.destroy();
         network = null;
         sources.videocdn.destroy();
-        sources.rezka.destroy();
-        sources.rezka2.destroy();
+        sources.cdnmovies.destroy();
         sources.kinobase.destroy();
         sources.collaps.destroy();
-        sources.cdnmovies.destroy();
+        sources.rezka.destroy();
+        sources.rezka2.destroy();
         sources.filmix.destroy();
         sources.hdvb.destroy();
         sources.anilibria.destroy();
@@ -7424,11 +7506,11 @@
         zh: '在线的'
       },
       online_mod_title_full: {
-        ru: 'Онлайн Мод',
-        uk: 'Онлайн Мод',
-        be: 'Анлайн Мод',
-        en: 'Online Mod',
-        zh: '在线的 Mod'
+        ru: 'Смотреть онлайн',
+        uk: 'Дивитися онлайн',
+        be: 'Глядзець анлайн',
+        en: 'Watch online',
+        zh: '在线观看'
       },
       online_mod_proxy_other: {
         ru: 'Использовать альтернативный прокси',
@@ -7660,11 +7742,32 @@
         be: 'Узнікла памылка',
         en: 'An error has occurred',
         zh: '发生了错误'
+      },
+      online_change_balanser: {
+        ru: 'Изменить балансер',
+        uk: 'Змінити балансер',
+        en: 'Change balancer',
+        zh: '更改平衡器'
+      },
+      online_balanser_dont_work: {
+        ru: 'Балансер ({balanser}) не отвечает на запрос.',
+        uk: 'Балансер ({balanser}) не відповідає на запит.',
+        en: 'Balancer ({balanser}) does not respond to the request.',
+        zh: '平衡器（{balanser}）未响应请求。'
+      },
+      online_balanser_timeout: {
+        ru: 'Балансер будет переключен автоматически через <span class="timeout">10</span> секунд.',
+        uk: 'Балансер буде переключено автоматично через <span class="timeout">10</span> секунд.',
+        en: 'Balancer will be switched automatically in <span class="timeout">10</span> seconds.',
+        zh: '平衡器将在<span class="timeout">10</span>秒内自动切换。'
       }
     });
+    Lampa.Template.add('online_mod_css', "\n        <style>\n        @charset 'UTF-8';.online-empty{line-height:1.4}.online-empty__title{font-size:1.8em;margin-bottom:.3em}.online-empty__time{font-size:1.2em;font-weight:300;margin-bottom:1.6em}.online-empty__buttons{display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex}.online-empty__buttons>*+*{margin-left:1em}.online-empty__button{background:rgba(0,0,0,0.3);font-size:1.2em;padding:.5em 1.2em;-webkit-border-radius:.2em;-moz-border-radius:.2em;border-radius:.2em;margin-bottom:2.4em}.online-empty__button.focus{background:#fff;color:black}.online-empty__templates .online-empty-template:nth-child(2){opacity:.5}.online-empty__templates .online-empty-template:nth-child(3){opacity:.2}.online-empty-template{background-color:rgba(255,255,255,0.3);padding:1em;display:-webkit-box;display:-webkit-flex;display:-moz-box;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-moz-box-align:center;-ms-flex-align:center;align-items:center;-webkit-border-radius:.3em;-moz-border-radius:.3em;border-radius:.3em}.online-empty-template>*{background:rgba(0,0,0,0.3);-webkit-border-radius:.3em;-moz-border-radius:.3em;border-radius:.3em}.online-empty-template__ico{width:4em;height:4em;margin-right:2.4em}.online-empty-template__body{height:1.7em;width:70%}.online-empty-template+.online-empty-template{margin-top:1em}\n        </style>\n    ");
+    $('body').append(Lampa.Template.get('online_mod_css', {}, true));
 
     function resetTemplates() {
       Lampa.Template.add('online_mod', "<div class=\"online selector\">\n        <div class=\"online__body\">\n            <div style=\"position: absolute;left: 0;top: -0.3em;width: 2.4em;height: 2.4em\">\n                <svg style=\"height: 2.4em; width:  2.4em;\" viewBox=\"0 0 128 128\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                    <circle cx=\"64\" cy=\"64\" r=\"56\" stroke=\"white\" stroke-width=\"16\"/>\n                    <path d=\"M90.5 64.3827L50 87.7654L50 41L90.5 64.3827Z\" fill=\"white\"/>\n                </svg>\n            </div>\n            <div class=\"online__title\" style=\"padding-left: 2.1em;\">{title}</div>\n            <div class=\"online__quality\" style=\"padding-left: 3.4em;\">{quality}{info}</div>\n        </div>\n    </div>");
+      Lampa.Template.add('modss_does_not_answer', "<div class=\"online-empty\">\n            <div class=\"online-empty__title\">\n                #{online_balanser_dont_work}\n            </div>\n            <div class=\"online-empty__time\">\n                #{online_balanser_timeout}\n            </div>\n            <div class=\"online-empty__buttons\">\n                <div class=\"online-empty__button selector cancel\">#{cancel}</div>\n                <div class=\"online-empty__button selector change\">#{online_change_balanser}</div>\n            </div>\n            <div class=\"online-empty__templates\">\n                <div class=\"online-empty-template\">\n                    <div class=\"online-empty-template__ico\"></div>\n                    <div class=\"online-empty-template__body\"></div>\n                </div>\n                <div class=\"online-empty-template\">\n                    <div class=\"online-empty-template__ico\"></div>\n                    <div class=\"online-empty-template__body\"></div>\n                </div>\n                <div class=\"online-empty-template\">\n                    <div class=\"online-empty-template__ico\"></div>\n                    <div class=\"online-empty-template__body\"></div>\n                </div>\n            </div>\n        </div>");
       Lampa.Template.add('online_mod_folder', "<div class=\"online selector\">\n        <div class=\"online__body\">\n            <div style=\"position: absolute;left: 0;top: -0.3em;width: 2.4em;height: 2.4em\">\n                <svg style=\"height: 2.4em; width:  2.4em;\" viewBox=\"0 0 128 112\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                    <rect y=\"20\" width=\"128\" height=\"92\" rx=\"13\" fill=\"white\"/>\n                    <path d=\"M29.9963 8H98.0037C96.0446 3.3021 91.4079 0 86 0H42C36.5921 0 31.9555 3.3021 29.9963 8Z\" fill=\"white\" fill-opacity=\"0.23\"/>\n                    <rect x=\"11\" y=\"8\" width=\"106\" height=\"76\" rx=\"13\" fill=\"white\" fill-opacity=\"0.51\"/>\n                </svg>\n            </div>\n            <div class=\"online__title\" style=\"padding-left: 2.1em;\">{title}</div>\n            <div class=\"online__quality\" style=\"padding-left: 3.4em;\">{quality}{info}</div>\n        </div>\n    </div>");
     }
 
@@ -7855,7 +7958,7 @@
     Lampa.Params.trigger('online_mod_proxy_other', false);
     Lampa.Params.trigger('online_mod_proxy_videocdn', false);
     Lampa.Params.trigger('online_mod_proxy_rezka', false);
-    Lampa.Params.trigger('online_mod_proxy_rezka2', false);
+    Lampa.Params.trigger('online_mod_proxy_rezka2', true);
     Lampa.Params.trigger('online_mod_proxy_kinobase', false);
     Lampa.Params.trigger('online_mod_proxy_collaps', false);
     Lampa.Params.trigger('online_mod_proxy_cdnmovies', false);
