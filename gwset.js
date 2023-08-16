ef17a986e31cad73d9daa728c69f524f02f8bea8
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-16 13:54:56 +0300

    Update gwset.js

--- gwset.js
+++ gwset.js
@@ -54,6 +54,73 @@
         }
 	});
 
+    var Modss = {
+        last_view: function (data) {
+			var episodes = Lampa.TimeTable.get(data);
+			var viewed;
+			episodes.forEach(function (ep) {
+				var hash = Lampa.Utils.hash([ep.season_number, ep.episode_number, data.original_title].join(''));
+				var view = Lampa.Timeline.view(hash);
+				if (view.percent) viewed = {
+					ep: ep,
+					view: view
+				};
+			});
+			if (viewed) {
+				var ep = viewed.ep.episode_number;
+				var se = viewed.ep.season_number;
+				var last_view = 'S' + se + ':E' + ep;
+				if ($('body').find('.full-start__buttons,.full-start-new__buttons').length) {
+					$('.timeline, .card--last_view').remove();
+					$('body').find('.full-start__left').append("<div class='card--last_view' style='top:0.6em;right: -.5em;position: absolute;background: #168FDF;color: #fff;padding: 0.3em 0.4em 0.4em;font-size: 1.2em;-webkit-border-radius: 0.3em;-moz-border-radius: 0.3em;border-radius: 0.3em;'><div style='float:left;margin:-4px 0 -4px -4px' class=''></div>" + last_view +"</div>");
+					$('body').find('.timeline').append(Lampa.Timeline.render(viewed.view));
+				}
+				if ($('body').find('.filter--sort').length) $('body').find('.files__left .time-line, .card--last_view').remove();
+			} else $('body').find('.timeline,.card--last_view').remove();
+			if ($('body').find('.online').length == 0) $('.card--new_ser,.card--viewed').remove();
+		},
+		serialInfo: function (card) {
+			if (Lampa.Storage.field('mods_serial_info') && card.source == 'tmdb' && card.seasons && card.last_episode_to_air) {
+				var last_seria_inseason = card.last_episode_to_air.season_number;
+				var air_new_episode = card.last_episode_to_air.episode_number;
+				var next_episode = card.next_episode_to_air;
+				var last_seria = next_episode && new Date(next_episode.air_date) <= Date.now() ? next_episode.episode_number : card.last_episode_to_air.episode_number;
+				var new_ser;
+				this.last_view(card);
+				var count_eps_last_seas = card.seasons.find(function (eps) {
+				 	return eps.season_number == last_seria_inseason;
+				}).episode_count;
+				
+				if (card.next_episode_to_air) {
+					var add_ = '<b>' + last_seria;
+					var notices = Lampa.Storage.get('account_notice', []).filter(function (n) {
+						return n.card_id == card.id;
+					});
+					if (notices.length) {
+						var notice = notices.find(function (itm) {
+						  return itm.episode == last_seria;
+						});
+						
+						if (notice) {
+  						var episod_new = JSON.parse(notice.data).card.seasons;
+  						if (Lampa.Utils.parseTime(notice.date).full == Lampa.Utils.parseTime(Date.now()).full) 
+  						add_ = '#{season_new} <b>' + episod_new[last_seria_inseason];
+  					} 
+					}
+					new_ser = add_ + '</b> #{torrent_serial_episode} #{season_from} ' + count_eps_last_seas + ' - Сезон ' + last_seria_inseason;
+				} else new_ser = last_seria_inseason + ' #{season_ended}';
+		
+				if(!$('.card--new_seria', Lampa.Activity.active().activity.render()).length) {
+  				if(window.innerWidth > 585) $('.full-start__left', Lampa.Activity.active().activity.render()).append("<div class='card--new_seria' style='right: -0.6em;position: absolute;background: #FF4242;color: #fff;bottom:.6em;padding: 0.3em 0.4em 0.4em;font-size: 1.2em;-webkit-border-radius: 0.3em;-moz-border-radius: 0.3em;border-radius: 0.3em;'>" + Lampa.Lang.translate(new_ser) + "</div>");
+  			  else {
+  			    if($('.card--new_seria', Lampa.Activity.active().activity.render()).length)$('.full-start__tags', Lampa.Activity.active().activity.render()).append('<div class="full-start__tag card--new_seria"><img src="./img/icons/menu/movie.svg" /> <div>'+ Lampa.Lang.translate(new_ser) +'</div></div>');
+  			    else $('.full-start-new__details', Lampa.Activity.active().activity.render()).append('<span class="full-start-new__split">●</span><div class="card--new_seria"><div>'+ Lampa.Lang.translate(new_ser) +'</div></div>');
+  			  }
+  		  }
+			}
+		}, 
+    }
+
     //Убирает общую кнопку Онлайн и достаёт все кнопки из блока
     /*Lampa.Listener.follow('full', function (e) {
         if (e.type == 'complite') {
