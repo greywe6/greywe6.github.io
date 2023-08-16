8e48d5cf3b1ce7500807d5eee60d80d63cfee5ce
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-16 09:36:15 +0300

    Update gwset.js

--- gwset.js
+++ gwset.js
@@ -41,7 +41,7 @@
             //Доп. стили кнопок
             Lampa.Template.add('stlico2_css', "\n   <style>\n .full-start-new__buttons .full-start__button.selector.view--online:not(.focus) span{display:block;}\n .full-start-new__buttons .full-start__button.selector.view--torrent:not(.focus) span{display:block;}\n .full-start-new__buttons .full-start__button.selector.view--trailer:not(.focus) span{display:block;}\n .full-start__button.selector.view--torrent svg{color:#76b83f;}\n .full-start__button.selector.view--trailer svg{color:#FF4242;}\n .full-start__button.selector.view--online svg{color:#168FDF;}\n   </style>");
             $('body').append(Lampa.Template.get('stlico2_css', {}, true));
-            Lampa.Template.add('rating_css', "\n   <style>\n .full-start__rate.rate--imdb{font-size: .75em;}\n .full-start__rate.rate--kp{font-size: .75em;}\n   </style>");
+            Lampa.Template.add('rating_css', "\n   <style>\n .full-start__rate.rate--imdb{font-size: 3.2em;font-weight: 700;line-height: 1.2;}\n .full-start__rate.rate--kp{font-size: 3.2em;font-weight: 700;line-height: 1.2;}\n   </style>");
             $('body').append(Lampa.Template.get('rating_css', {}, true));
             //Прячем ненужные разделы в МЕНЮ
             setTimeout(function(){
