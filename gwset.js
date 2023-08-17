3747c462f353ac82ae97003ec4e5d68c53cb8ec0
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-17 13:37:22 +0300

    Update gwset.js

--- gwset.js
+++ gwset.js
@@ -45,6 +45,12 @@
             Lampa.Template.add('rating_mod_css', "\n   <style>\n .full-start__tag.tag--countries:empty{display: none!important;}\n .full-start__title-original{color:#eeeeee;}\n .full-start__tags{padding-bottom: 2.1em!important;}\n .info__rate{background: rgba(0,0,0,.26)!important;}\n .full-start__rate{margin: 0;border-radius: 1em;font-size: .75em;margin-right: 1.5em;position: relative;}\n .full-start__rate>div:first-child{background: rgba(0,0,0,.1)!important;font-size: 3.2em;font-weight: 700;line-height: 1.2;padding: 0.77em 1.01em 0.92em;}\n div{display:block;}\n .full-start__rate>div:last-child{position: absolute;bottom: -0.6em;right: 0;background-color: #171717;color: #fff;padding: 0.31em 0.33em 0.33em 0.34em;border-radius: 0.23em;font-size: 1.0em;}\n .full-start .info__rate>div{background-color: #171717;color: #fff;}\n  </style>");
             $('body').append(Lampa.Template.get('rating_mod_css', {}, true));
 
+            if($(".tag--countries").is(":empty")) {
+	
+                $(".tag--countries").addClass("hide");
+            
+            }
+
 
             //Прячем ненужные разделы в МЕНЮ
             setTimeout(function(){
@@ -59,9 +65,9 @@
 
     $(document).ready(function() {
 	
-		if($(".full-start__tag.tag--countries").is(":empty")) {
+		if($(".tag--countries").is(":empty")) {
 	
-			$(".full-start__tag.tag--countries").addClass("hide");
+			$(".tag--countries").addClass("hide");
 		
 		}
 		
