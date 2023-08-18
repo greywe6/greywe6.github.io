d5302e0df261cf751fb3750c0794dd09d358cbb2
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-18 18:58:05 +0300

    Update gwtest.js

--- gwtest.js
+++ gwtest.js
@@ -2,9 +2,10 @@
 $.fn.colorize = function () {
    return this.each(function() {
       var $this = $(this), number = $this.text();
-      $this.css({color: number < 5 ? "red"
-                      : number < 10 ? "green"
-                      : number = 7 ? "yellow"});
-});
+   	  $this.css({color: number < 15 ? "red"
+                      : number < 45 ? "green"
+                      : "yellow"});
+   });
 };
-$("#app > div.wrap.layer--height.layer--width > div.wrap__content.layer--height.layer--width > div > div > div > div.activity__body > div > div > div > div > div.full-start > div.full-start__body > div.full-start__left > div.full-start__deta > div.info__rate > span").colorize();
+
+$("div.info__rate > span").colorize();
