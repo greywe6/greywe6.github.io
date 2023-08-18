400a99bfa8a4b0413646d993fa5eddbb7d3cbb41
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-18 18:52:38 +0300

    Update gwtest.js

--- gwtest.js
+++ gwtest.js
@@ -1,10 +1,12 @@
 //НАСТРОЙКИ ПРОГРАММЫ LAMPA by greyweb
 (function () {
-   $("#app > div.wrap.layer--height.layer--width > div.wrap__content.layer--height.layer--width > div > div > div > div.activity__body > div > div > div > div > div.full-start > div.full-start__body > div.full-start__left > div.full-start__deta > div.info__rate > span").each(function() {
-  $(this).html() < 3 ? $(this).css('color', 'red') : null;
-
-  ($(this).html() >= 7.0 && $(this).html() < 10) ? $(this).css('color', 'green'): null;
-
-  $(this).html() >= 3 ? $(this).css('color', 'yellow') : null;
+   $.fn.colorize = function () {
+   return this.each(function() {
+      var $this = $(this), number = $this.text();
+      $this.css({color: number < 5 ? "red"
+                      : number < 10 ? "green"
+                      : number = 7 ? "yellow"});
 });
+};
+   $("#app > div.wrap.layer--height.layer--width > div.wrap__content.layer--height.layer--width > div > div > div > div.activity__body > div > div > div > div > div.full-start > div.full-start__body > div.full-start__left > div.full-start__deta > div.info__rate > span").colorize();
 })();
