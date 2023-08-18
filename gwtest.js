c3322787819016e2951cb77e06c87d6524df2a86
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-18 23:05:22 +0300

    Update gwtest.js

--- gwtest.js
+++ gwtest.js
@@ -1,10 +1,9 @@
 (function () {
-     ;[].forEach.call(document.querySelectorAll(".info__rate span"),function(elem){
-    var arr={
-        7.0:"blue",
-        6.0:"green",
-        5.0:"gold"
-    }
-    elem.style.backgroundColor=arr[elem.innerHTML.toLowerCase().trim()]
-})
+         $(document).ready(function(){
+        var value = parseInt($('.info__rate span').html());
+      
+        if (value >= 7.0){
+           $('.info__rate span').css("color","red");
+        }
+      });
 }();
