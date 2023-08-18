fe3274a1e4c112be90b21e0d5311f4a22d44351d
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-18 23:07:21 +0300

    Update gwtest.js

--- gwtest.js
+++ gwtest.js
@@ -1,9 +1,8 @@
-(function () {
-         $(document).ready(function(){
+$(document).ready(function(){
         var value = parseInt($('.info__rate span').html());
       
         if (value >= 7.0){
            $('.info__rate span').css("color","red");
         }
       });
-}();
+         
