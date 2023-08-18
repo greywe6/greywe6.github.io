960fb8b02e37b54c0ec375d2aaa10ebeb8385db1
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-18 18:54:48 +0300

    Update gwtest.js

--- gwtest.js
+++ gwtest.js
@@ -1,6 +1,5 @@
 //НАСТРОЙКИ ПРОГРАММЫ LAMPA by greyweb
-(function () {
-   $.fn.colorize = function () {
+$.fn.colorize = function () {
    return this.each(function() {
       var $this = $(this), number = $this.text();
       $this.css({color: number < 5 ? "red"
@@ -8,5 +7,4 @@
                       : number = 7 ? "yellow"});
 });
 };
-   $("#app > div.wrap.layer--height.layer--width > div.wrap__content.layer--height.layer--width > div > div > div > div.activity__body > div > div > div > div > div.full-start > div.full-start__body > div.full-start__left > div.full-start__deta > div.info__rate > span").colorize();
-})();
+$("#app > div.wrap.layer--height.layer--width > div.wrap__content.layer--height.layer--width > div > div > div > div.activity__body > div > div > div > div > div.full-start > div.full-start__body > div.full-start__left > div.full-start__deta > div.info__rate > span").colorize();
