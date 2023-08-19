ea8800e5c088a6dad6c09ce66fa00a498cd35f9e
G R Σ Ý <110333010+greywe6@users.noreply.github.com>
2023-08-19 13:50:35 +0300

    Update gwset.js

--- gwset.js
+++ gwset.js
@@ -29,6 +29,7 @@
     //Украшение и дополнение стилей
 	Lampa.Listener.follow('app', function(e) {
 	    if(e.type == 'ready') { 
+            $(document).ready(function() { $('div:empty').remove(); }); 
             //Удалить кнопку рекламы Премиум в шапке
             $('#app > div.head > div > div.head__actions > .open--premium').remove();
 	        //Удалить кнопку Лента в шапке
